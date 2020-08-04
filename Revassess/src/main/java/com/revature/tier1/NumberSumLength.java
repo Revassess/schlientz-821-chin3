63ba512467be5219d42ee02a9e1447bc23f56046
nathan <nathannchin@gmail.com>
2020-08-04 13:44:09 -0500

    Adding tier1 code

--- Revassess/src/main/java/com/revature/tier1/NumberSumLength.java
+++ Revassess/src/main/java/com/revature/tier1/NumberSumLength.java
@@ -3,6 +3,24 @@ package com.revature.tier1;
 public class NumberSumLength {
 
 	public static boolean checkNumberPowerLength(long num) {
+		long powerNum = 0;
+		String stringnum = Long.toString(num);
+		if (num == 0) {
+			return true;
+		} else {
+
+			for (int i = 0; i < stringnum.length(); i++) {
+				char c = stringnum.charAt(i);
+				long cnum = Long.parseLong((Character.toString(c)));
+				powerNum += (Math.pow(cnum, stringnum.length()));
+			}
+			if (powerNum == num)
+				return true;
+		}
 		return false;
+	}
+	public static void main(String[] args) {
+		System.out.println(checkNumberPowerLength(1));
+		System.out.println(checkNumberPowerLength(200));
 	}
 }
--- Revassess/src/main/java/com/revature/tier1/SumOverArray.java
+++ Revassess/src/main/java/com/revature/tier1/SumOverArray.java
@@ -3,6 +3,18 @@ package com.revature.tier1;
 public class SumOverArray {
 
 	public static int IterateAndSum(int[] arr) {
-		return 0;
+		int sum = 0;
+		if (arr.length == 0)
+				return 0;
+		else {
+			for(int n : arr) {
+				sum +=n;
+			}
+			return sum;
+		}
+	}
+	public static void main(String args[]) {
+		int[] arr = {};
+		System.out.println(IterateAndSum(arr));
 	}
 }
